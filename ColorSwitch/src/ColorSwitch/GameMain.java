da14ad0960aad0c1c7ca894fd88fe83d7317de49
pritish19076 <pritish19076@iiitd.ac.in>
2020-11-13 17:08:19 +0530

    Simplified Code by Using Polymorphism

--- ColorSwitch/src/ColorSwitch/GameMain.java
+++ ColorSwitch/src/ColorSwitch/GameMain.java
@@ -7,6 +7,7 @@ import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
@@ -16,9 +17,29 @@ import javafx.util.Duration;
 
 
 public class GameMain extends Application {
+    private boolean loadgame = false;
+
+    private TranslateTransition loadinAnimation(boolean reverse, double duration) {
+        TranslateTransition load = new TranslateTransition();
+        if (!reverse) load.setByY(683);
+        else load.setByY(-683);
+        load.setNode(LoadGameWindowGroup);
+        load.setDuration(Duration.millis(duration));
+        return load;
+    }
+
+    private FadeTransition fade(Node n, boolean fadein) {
+        FadeTransition fadeload = new FadeTransition();
+        fadeload.setDuration(Duration.millis(1000));
+        if (fadein) fadeload.setToValue(1);
+        else fadeload.setToValue(0);
+        fadeload.setNode(n);
+        return fadeload;
+
+    }
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("ColorSwitch.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1280, 720));
@@ -26,6 +47,7 @@ public class GameMain extends Application {
         primaryStage.show();
 
     }
+
     @FXML
     private Group MainMenuGroup;
 
@@ -44,31 +66,20 @@ public class GameMain extends Application {
     @FXML
     void closeLoadGame(MouseEvent event) {
         System.out.println("close");
-        FadeTransition fadeload = new FadeTransition();
-        fadeload.setDuration(Duration.millis(1000));
-        fadeload.setToValue(0);
-        fadeload.setNode(LoadGameWindowGroup);
-        TranslateTransition load = new TranslateTransition();
-        load.setByY(-683);
-        load.setNode(LoadGameWindowGroup);
-        load.setDuration(Duration.millis(1000));
-        new SequentialTransition(fadeload,load).play();
-
-
+        if (loadgame) {
+            new SequentialTransition(fade(LoadGameWindowGroup, false), loadinAnimation(true,1)).play();
+            loadgame = false;
+        }
     }
 
     @FXML
     void loadGame(MouseEvent event) {
         System.out.println("Load");
-        if(LoadGameWindowGroup.getLayoutY()<0) {
+        if (!loadgame) {
             LoadGameWindowGroup.setOpacity(1);
             System.out.println("If ke andar");
-            TranslateTransition load = new TranslateTransition();
-            load.setByY(683);
-            load.setNode(LoadGameWindowGroup);
-            load.setDuration(Duration.millis(1000));
-            load.play();
-
+            loadinAnimation(false,1000).play();
+            loadgame = true;
         }
 
 
@@ -78,10 +89,10 @@ public class GameMain extends Application {
     void startGame(MouseEvent event) {
 
     }
+
     public static void main(String[] args) {
         launch(args);
     }
-
 
 
 }
