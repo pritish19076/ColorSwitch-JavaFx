f45208f60f66ae63ab999ada19abddaaccc81e41
pritish19076 <pritish19076@iiitd.ac.in>
2020-11-16 23:44:20 +0530

    Implemented Acceleration Physics

--- ColorSwitch/src/ColorSwitch/GamePlayController.java
+++ ColorSwitch/src/ColorSwitch/GamePlayController.java
@@ -34,20 +34,21 @@ public class GamePlayController implements Initializable {
     private boolean gameStarted=false;
     boolean found = false;
     int count = 0;
-
+    double speedX=0;
+    double speedY=0;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("dsa");
-        currentBall = new Ball(263,707,20,1,3,0);
+        currentBall = new Ball(263,707,15,1,3,0);
         currentBall.display(gamePlayAnchorPane);
         gameObstacles = new ArrayList<>();
         Obstacles obs1 = new NormalCircle(3000,true,100.0f,100.0f,263,440);
         gameObstacles.add(obs1);
-        Obstacles obs2 = new NormalCircle(3000,true,100.0f,100.0f,263,40);
+        Obstacles obs2 = new NormalCircle(3000,true,100.0f,100.0f,263,20);
         gameObstacles.add(obs2);
         obs1.display(gamePlayAnchorPane);
         obs2.display(gamePlayAnchorPane);
-        Obstacles obs3 = new NormalCircle(3000,true,100.0f,100.0f,263,-360);
+        Obstacles obs3 = new NormalCircle(3000,true,100.0f,100.0f,263,-380);
         gameObstacles.add(obs3);
         obs3.display(gamePlayAnchorPane);
         CommonAnimation.fade(currentBall.getGameBall(),1).play();
@@ -85,8 +86,10 @@ public class GamePlayController implements Initializable {
             gravity.setCycleCount(Animation.INDEFINITE);
 
             KeyFrame grav=new KeyFrame(Duration.millis(20),e -> {
-                currentBall.getGameBall().setCenterY(currentBall.getGameBall().getCenterY()+3);
-                if(Y_Ball+20<prevY_Ball){
+                //currentBall.getGameBall().setCenterY(currentBall.getGameBall().getCenterY()+3);
+                update();
+                System.out.println("speed "+speedY);
+                /*if(Y_Ball+20<prevY_Ball){
                     System.out.println(prevY_Ball-Y_Ball);
                    // System.out.println("Yball: "+Y_Ball+ "prevY: "+ prevY_Ball );
                     prevY_Ball=currentBall.getGameBall().getCenterY();
@@ -94,20 +97,44 @@ public class GamePlayController implements Initializable {
                     for(Obstacles o: gameObstacles)
                     {
 
-                        o.getGroup().setLayoutY(o.getGroup().getLayoutY()+60);
+                        o.getGroup().setLayoutY(o.getGroup().getLayoutY()+65);
                     }
                    // Y_Ball=currentBall.getGameBall().getCenterY()-100;
 
                 }
                 Y_Ball=currentBall.getGameBall().getCenterY();
-                count++;
+                //count++;
                // System.out.println("BaharkaYball: "+Y_Ball);
+                */
+                if(speedY<-0.1)
+                {
+                    for(Obstacles o: gameObstacles)
+                    {
 
+                        o.getGroup().setLayoutY(o.getGroup().getLayoutY()+1.2);
+                    }
+
+                }
             });
             gravity.getKeyFrames().add(grav);
             gravity.play();
 
     }
+    public void accelerate(double accelerationY) {
+
+        speedY += accelerationY;
+    }
+
+    public void move(double yDelta) {
+        currentBall.getGameBall().setCenterY(currentBall.getGameBall().getCenterY()+yDelta);
+
+    }
+
+    public void update() {
+        move(speedY);
+        accelerate( 0.05); // gravity accelerates the object downwards each tick Range - 0.03 to 0.04
+    }
+
     public void setupScene(Scene p_scene) {
         System.out.println("initial");
         gamePlayScene = p_scene;
@@ -121,7 +148,10 @@ public class GamePlayController implements Initializable {
                     Timeline move=new Timeline();
                     move.setCycleCount(15);
                     KeyFrame mov=new KeyFrame(Duration.millis(2),e -> {
-                        currentBall.getGameBall().setCenterY(currentBall.getGameBall().getCenterY()-5);
+
+                        //accelerate(-0.06);
+                       speedY-=0.05; //Range From 0.05 to 0.08
+                        //currentBall.getGameBall().setCenterY(currentBall.getGameBall().getCenterY()-5);
                     });
                     move.getKeyFrames().add(mov);
                     move.play();
